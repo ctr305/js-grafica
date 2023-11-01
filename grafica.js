7b38346698b8977e7484e795f2d00bec6d177a73
Leonardo Criollo <criollo_banneret@tutanota.com>
2023-10-31 19:34:21 -0600

    Arreglar javascript.

--- grafica.js
+++ grafica.js
@@ -1,47 +1,68 @@
-const labels = ['1 año', '2 años'];
+const labels = ["1 año", "2 años"];
 const datos = [24000, 27000];
 const colores = [
-    'rgba(255, 99, 132)',
-    'rgba(255, 159, 64)',
-    'rgba(255, 205, 86)',
-    'rgba(75, 192, 192)',
-    'rgba(54, 162, 235)',
-    'rgba(153, 102, 255)',
-    'rgba(201, 203, 207)'
+    "rgba(255, 99, 132)",
+    "rgba(255, 159, 64)",
+    "rgba(255, 205, 86)",
+    "rgba(75, 192, 192)",
+    "rgba(54, 162, 235)",
+    "rgba(153, 102, 255)",
+    "rgba(201, 203, 207)",
 ];
 
 const data = {
     labels: labels,
-    datasets: [{
-        label: 'Mis Inversiones',
-        data: datos,
-        backgroundColor: colores,
-        borderColor: colores,
-        borderWidth: 1
-    }]
+    datasets: [
+        {
+            label: "Mis Inversiones",
+            data: datos,
+            backgroundColor: colores,
+            borderColor: colores,
+            borderWidth: 1,
+        },
+    ],
 };
 
 const config = {
-    type: 'bar',
+    type: "bar",
     data: data,
     plugins: [ChartDataLabels],
     options: {
         plugins: {
             legend: {
-                display: false
+                display: false,
             },
             dataLabels: {
-                anchor: 'end',
+                anchor: "end",
                 clamp: true,
-                align: 'top',
+                align: "top",
                 offset: 0,
-                formatter: function(value, context) {
-                    return '$' + new Intl.NumberFormat('es-MX').format(context.dataset.data[context.dataIndex]);
-                }
-            }
-        }
+                formatter: function (value, context) {
+                    return (
+                        "$" +
+                        new Intl.NumberFormat("es-MX").format(
+                            context.dataset.data[context.dataIndex],
+                        )
+                    );
+                },
+            },
+        },
     },
 };
 
-const ctx = document.querySelector('.grafica');
+const ctx = document.querySelector(".grafica");
 const graficaInvest = new Chart(ctx, config);
+
+const slider = document.querySelector(".slider");
+const ganancias = [24000, 27000, 34000, 40500, 46300, 48000, 54500];
+
+slider.addEventListener("change", function () {
+    graficaInvest.data.labels = ["1 año"];
+    graficaInvest.data.datasets[0].data = [24000];
+
+    for (i = 2; i <= this.value; i++) {
+        graficaInvest.data.labels.push(`${i} años`);
+        graficaInvest.data.datasets[0].data.push(ganancias[i - 1]);
+    }
+    graficaInvest.update();
+});
